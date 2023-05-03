74f5867351d865e6b6c17b9587a8f05cdbf99110
김민주 <kimminju@gimminjuui-MacBookPro.local>
2023-05-03 21:36:44 +0900

    Add teamp

---
+++ _teamp/src/chap_01/_01_MultipleTable.java
@@ -0,0 +1,13 @@
+//Java학습 구구단 프로그램 개발 2023.05.03(수)
+package chap_01;
+
+public class _01_MultipleTable {
+    public static void main(String [] args){
+        for(int i=2; i<10; i++){
+            for(int j=1; j<10; j++){
+                System.out.println(i + "x" + j + "=" + (i*j));
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ _teamp/src/chap_01/_02_Study.java
@@ -0,0 +1,6 @@
+//Java학습 수업시간내 예제 정리
+package chap_01;
+
+public class _02_Study {
+    
+}
